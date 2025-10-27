916bdeedbccef1fe628936f95263b7af215fac1a
tru897tr <truongthikimthanh.cx261984@gmail.com>
2025-10-27 08:57:45 +0700

    Update install-pyenv.sh

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -4,20 +4,22 @@ set -e
 echo "CÀI pyenv + Python 3.12..."
 
 # Cài pyenv
-curl https://pyenv.run | bash
+if [ ! -d "$HOME/.pyenv" ]; then
+    curl https://pyenv.run | bash
+fi
 
-# Thêm vào PATH
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# Cập nhật PATH
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
 # Cài Python 3.12.7
-pyenv install 3.12.7 -s
-pyenv global 3.12.7
+~/.pyenv/bin/pyenv install 3.12.7 -s
+~/.pyenv/bin/pyenv global 3.12.7
 
-# Tạo alias
+# Alias
 echo 'alias python="python3.12"' >> ~/.bashrc
 echo 'alias pip="python3.12 -m pip"' >> ~/.bashrc
 
-echo "Python 3.12 đã sẵn sàng!"
-python --version
+echo "Python 3.12 đã cài!"
